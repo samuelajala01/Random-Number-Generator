07dc22c7807fa0cb97d7458a2dd4986ccb70b40b
Samuel Ajala <samuelajala01@gmail.com>
2021-10-20 18:10:25 +0100

    first init

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function clear() {
-  document.getElementById('input').value = '';
+  document.getElementById('input').innerHTML = '';
   document.getElementById('result').innerHTML = '';
 }
 
@@ -7,15 +7,13 @@ document.getElementById('btn-1').addEventListener('click', function(e) {
   const a = document.getElementById('input').value;
   let num = Math.ceil(Math.random()* a);
 
-  if(a === '' ) {
+
+  if(a === '') {
     document.getElementById('result').innerHTML = '';
-    alert('Type a number')
+    alert('Enter a number')
   } else{
     document.getElementById('result').innerHTML = num;
 
   }
   e.preventDefault();
 })
-
-
-
